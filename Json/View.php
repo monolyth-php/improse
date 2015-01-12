24d32ac489b39e8259a3ca2847423c520e9f23e1
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-12 10:34:45 +0100

    json view

---
+++ Json/View.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace improse\Json;
+use improse;
+
+class View extends improse\View
+{
+    private $jsonData;
+
+    public function __construct($data)
+    {
+        $this->jsonData = $data;
+    }
+
+    public function __invoke()
+    {
+        header("Content-type: application/json", true);
+        /*
+        if (isset($_SERVER['HTTP_ORIGIN'])
+            && in_array($_SERVER['HTTP_ORIGIN'], $project['origins'])
+        ) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        } else {
+            header("Access-Control-Allow-Origin: {$project['http']}");
+        }
+        */
+        header("Access-Control-Allow-Headers: X-Requested-With");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        echo json_encode($this->jsonData, JSON_NUMERIC_CHECK);
+    }
+}
+
