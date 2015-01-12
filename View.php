8bd20570847d30301e6b8c8a565fb8b5b3de4584
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-12 10:32:47 +0100

    add utility to user $_template and $_exports

--- View.php
+++ View.php
@@ -4,5 +4,19 @@ namespace improse;
 
 abstract class View
 {
+    protected $_template;
+    protected $_exports = [];
+
+    public function __invoke()
+    {
+        if ($this->_exports) {
+            foreach ($this->_exports as $__var) {
+                $$__var = $this->$__var;
+            }
+        }
+        if (isset($this->_template)) {
+            include $this->_template;
+        }
+    }
 }
 
