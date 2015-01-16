c2b2c58f039bebde6db91ca2392c32c8ec86af4a
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-16 12:14:06 +0100

    add default invoke for html templates

--- Html/View.php
+++ Html/View.php
@@ -5,6 +5,10 @@ use improse;
 
 class View extends improse\View
 {
-    
+    public function __invoke()
+    {
+        header("Content-type: text/html; charset=utf-8");
+        parent::__invoke();
+    }
 }
 
