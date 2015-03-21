57ac94986a21a4cbaaab54781d428744179dde2c
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-21 16:39:36 +0100

    rewrite tests to reflect new, improved functionality

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -15,7 +15,7 @@ class TestView2 extends View
 {
     public function __invoke()
     {
-        $out = new Html(realpath(__DIR__).'/helloworld.php');
+        $out = new Html(dirname(__FILE__).'/_files/helloworld.php');
         return $out();
     }
 }
@@ -32,19 +32,18 @@ class ViewTest extends PHPUnit_Framework_TestCase
     public function testViewWithHtml()
     {
         $view = new TestView2;
-        ob_start();
-        $view();
-        $out = ob_get_clean();
+        $out = $view();
         $this->assertEquals("<h1>Hello world!</h1>\n", $out);
     }
 
     public function testViewTemplate()
     {
         $view = new TestView2;
-        $template = new Html(realpath(__DIR__).'/template.php');
-        ob_start();
-        $template(['helloWorld' => $view]);
-        $out = ob_get_clean();
+        $template = new Html(
+            dirname(__FILE__).'/_files/template.php',
+            ['helloWorld' => $view]
+        );
+        $out = $template();
         $this->assertEquals(<<<EOT
 <div>
     <h1>Hello world!</h1>
