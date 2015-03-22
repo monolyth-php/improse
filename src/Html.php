99f07c7e1738998fa95879597f8e5731e8935ed0
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-22 11:32:45 +0100

    support returning of variables from templates, yay

--- src/Html.php
+++ src/Html.php
@@ -12,7 +12,20 @@ class Html extends View
         return call_user_func(function () {
             extract($this->viewdata);
             ob_start();
-            require $this->template;
+            $return = require $this->template;
+            if ($return && is_array($return)) {
+                foreach ($return as $key => $value) {
+                    if (array_key_exists($key, static::$globalViewdata)
+                        && is_null(static::$globalViewdata[$key])
+                    ) {
+                        unset(static::$globalViewdata[$key]);
+                    }
+                }
+                static::$globalViewdata = array_merge_recursive(
+                    static::$globalViewdata,
+                    $return
+                );
+            }
             return ob_get_clean();
         });
     }
