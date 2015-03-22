07e78e6fa1928a2635c1b58d329307f668e57e8d
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-22 09:53:53 +0100

    cleanup/simplify

--- src/Html.php
+++ src/Html.php
@@ -6,12 +6,11 @@ class Html extends View
 {
     protected $template;
 
-    public function __invoke(array $__viewdata = [])
+    public function __invoke(array $viewdata = [])
     {
-        $__viewdata += $this->viewdata;
-        return call_user_func(function() use ($__viewdata) {
-            extract($__viewdata);
-            unset($__viewdata);
+        $this->viewdata = $viewdata + $this->viewdata;
+        return call_user_func(function () {
+            extract($this->viewdata);
             ob_start();
             require $this->template;
             return ob_get_clean();
