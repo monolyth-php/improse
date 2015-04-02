9107cf8bde9e2fdaf6fcc520c38893770685c2ad
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-02 08:16:41 +0200

    add default views

--- src/Html.php
+++ src/Html.php
@@ -8,6 +8,21 @@ class Html extends View
 
     public function __invoke(array $viewdata = [])
     {
+        if (!isset($this->template)) {
+            $class = get_class($this);
+            if ($class{0} == '\\') {
+                $class = substr($class, 1);
+            }
+            $parts = explode('\\', $class);
+            $last = array_pop($parts);
+            if ($last == 'View') {
+                $last = 'template';
+            } else {
+                $last = strtolower(preg_replace('@View$@', '', $last));
+            }
+            $parts[] = $last;
+            $this->template = implode('/', $parts).'.php';
+        }
         $this->viewdata = $viewdata + $this->viewdata + self::$globalViewdata;
         return call_user_func(function () {
             extract($this->viewdata);
