eee68362592a7c2332e899803eb5c8bfc24a3ab5
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-06 08:15:01 +0200

    simplify this

--- src/Json.php
+++ src/Json.php
@@ -6,8 +6,8 @@ class Json extends View
 {
     public function __invoke(array $viewdata = [])
     {
-        $this->viewdata = $viewdata + $this->viewdata;
-        return json_encode($this->viewdata, JSON_NUMERIC_CHECK);
+        @header('Content-type: application/json', true, 200);
+        return json_encode($viewdata, JSON_NUMERIC_CHECK);
     }
 }
 
