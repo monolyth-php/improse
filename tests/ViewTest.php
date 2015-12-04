0144049fa3b0101a1069735f69d8b4d7f16d6232
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-04 09:52:43 +0100

    properly implement new tests

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -1,15 +1,57 @@
 <?php
 
-set_include_path(dirname(__FILE__).PATH_SEPARATOR.get_include_path());
-require_once dirname(__FILE__).'/Test/View1.php';
+namespace Improse\Test;
+
+use PHPUnit_Framework_TestCase;
+use Improse\View;
 
 class ViewTest extends PHPUnit_Framework_TestCase
 {
-    public function testView()
+    public function testHtmlView()
     {
-        $view = new Test\View1;
-        $out = "$view";
-        $this->assertEquals('Hello world!', $out);
+        $view = new View(__DIR__.'/_files/html.html');
+        $view->foo = 'bar';
+        $this->assertEquals('<h1>Hello world!</h1>', trim($view));
+    }
+
+    public function testPhpView()
+    {
+        $view = new View(__DIR__.'/_files/php.php');
+        $view->foo = 'bar';
+        $this->assertEquals('<h1>bar</h1>', trim($view));
+    }
+
+    public function testOverriddenEngine()
+    {
+        $view = new View(__DIR__.'/_files/php.php');
+        $view->foo = 'bar';
+        $view::$engine = function (array $variables) {
+            return strtoupper($this->foo);
+        };
+        $this->assertEquals('BAR', trim($view));
+    }
+
+    public function testExceptionTriggersWhoops()
+    {
+        $view = new View(__DIR__.'/_files/php.php');
+        $view::$engine = function (array $variables) {
+            throw new \ErrorException('testing');
+        };
+        $view->bar = 'foo';
+        $this->assertTrue(strpos(trim($view), '<div class="Whoops container">') !== false);
+        $this->assertTrue(View::whoops());
+    }
+
+    public function testSwallowErrors()
+    {
+        $view = new View(__DIR__.'/_files/php.php');
+        $view::$engine = function (array $variables) {
+            throw new \ErrorException('testing');
+        };
+        $view::$swallowErrors = 'This is an error';
+        $view->bar = 'foo';
+        $this->assertEquals('This is an error', trim($view));
+        $this->assertTrue(View::whoops());
     }
 }
 
