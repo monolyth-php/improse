072b1a354b9a16ed1b751c0ceb09e89940487f9d
Marijn Ophorst <marijn@monomelodies.nl>
2022-05-21 17:41:29 +0200

    remove the whoile Whoops thing as a default, it is not maintained and thus annoying

--- src/View.php
+++ src/View.php
@@ -7,45 +7,44 @@ use ReflectionClass;
 use ReflectionProperty;
 use Exception;
 use Closure;
-use Whoops\Run;
-use Whoops\Handler\PrettyPageHandler;
 
 class View
 {
     /**
-     * @var string
      * The template to render for this view. It should be "resolvable" by a call
      * to `include $this->template`, so either pass the full path or setup your
      * include paths/templating engine accordingly.
+     *
+     * @var string
      */
-    protected $template;
-
-    /**
-     * @var false|string
-     * If false (the default) shows a Whoops error page on error. If set to a
-     * string, displays that message instead. Extending views may define custom
-     * settings for this.
-     */
-    public static $swallowError = false;
+    protected ?string $template;
     
     /**
-     * @var bool
      * True if any view triggered an error. Accessible via static `whoops`
      * method. Use to decide e.g. if you want to terminate your script.
+     *
+     * @var bool
      */
-    private static $didWhoops = false;
+    private static bool $didWhoops = false;
+
+    /**
+     * Callable to handle errors.
+     *
+     * @var callable
+     */
+    private static $errorHandler;
 
     /**
      * Constructor. Optionally specify the template file. If omitted or null,
      * the $template property (or the class's parent's) is used.
      *
-     * @param string $template Resolvable path to template file. Default
+     * @param string|null $template Resolvable path to template file. Default
      *  resolve is simply `include`, other template engines might let you
      *  define a base path (e.g. Twig).
      * @throws DomainException if a template wasn't defined either as a property
      *  or during construction.
      */
-    public function __construct($template = null)
+    public function __construct(string $template = null)
     {
         if (isset($template)) {
             $this->template = $template;
@@ -55,6 +54,11 @@ class View
                 "Every view must define its \$template file."
             );
         }
+        if (!isset(self::$errorHandler)) {
+            self::setErrorHandler(function (Throwable $e) : string {
+                return 'Oopsie, we did a boo-boo :-(';
+            });
+        }
     }
 
     /**
@@ -62,40 +66,27 @@ class View
      *
      * @return string A string of HTML, e.g. to be `echo`'d.
      */
-    public function __toString()
+    public function __toString() : string
     {
-        static $run, $handler;
-        if (!isset($run, $handler)) {
-            $run = new Run;
-            $handler = new PrettyPageHandler;
-            $handler->handleUnconditionally(true);
-            $run->pushHandler($handler);
-            $run->allowQuit(false);
-            $run->writeToOutput(false);
-            $run->register();
-        }
         // __toString cannot throw an exception, so we need to handle those
         // manually to prevent PHP from barfing:
         try {
             return $this->render();
         } catch (Exception $e) {
             self::$didWhoops = true;
-            if (!static::$swallowError) {
-                return $run->handleException($e);
-            } else {
-                return static::$swallowError;
-            }
+            return call_user_func(self::$errorHandler, $e);
         }
     }
 
     /**
      * Render the template as a string. Use this instead of `__toString` to
-     * allow errors to be thrown instead of letting Whoops handle them.
+     * allow errors to be thrown instead of letting the errorHandler handle
+     * them.
      *
      * @return string A string of HTML, e.g. to be `echo`'d.
-     * @throws mixed Whatever error rendering the template might trigger.
+     * @throws Throwable Whatever error rendering the template might trigger.
      */
-    public function render()
+    public function render() : string
     {
         extract($this->getVariables());
         unset($__variables);
@@ -105,12 +96,24 @@ class View
     }
 
     /**
+     * Set your preferred error handler for this project.
+     *
+     * @param callable $errorHandler Receives the thrown exception as its only
+     *  argument.
+     * @return void
+     */
+    public static function setErrorHandler(callable $errorHandler) : void
+    {
+        self::$errorHandler = $errorHandler;
+    }
+
+    /**
      * Internal method to get all template variables, i.e. the public properties
      * on the view class.
      *
      * @return array A hash of key/value pairs.
      */
-    protected function getVariables()
+    protected function getVariables() : array
     {
         $reflection = new ReflectionClass($this);
         foreach ($reflection->getProperties(
@@ -143,7 +146,7 @@ class View
      * @return boolean True if there was at least one error, false if everything
      *  was hunky dory.
      */
-    public static function whoops()
+    public static function whoops() : bool
     {
         return self::$didWhoops;
     }
